b904bc057d3a71f30941189ee0518d0d3be8f0a0
zwnpepsi <1076168822@qq.com>
2017-10-30 14:47:51 +0800

    更新作业内容

--- homework/homework_1028_excel_nationalday_config/run_mysql.py
+++ homework/homework_1028_excel_nationalday_config/run_mysql.py
@@ -11,13 +11,13 @@ from homework.homework_1028_excel_nationalday_config.readexcel import ReadExcel
 from homework.homework_1028_excel_nationalday_config.write_mysql import WriteMysql
 from homework.homework_1028_excel_nationalday_config.read_mysql import ReadMysql
 
-class Run:
+class RunMysql:
     def __init__(self,result):
         self.result=result
         self.http_request_obj = HttpRequest("http://119.23.241.154:8080")  # 生成一个http请求实例
         self.write_result=WriteMysql()
 
-    def run(self):
+    def run_mysql(self):
         for i in range(len(self.result)):
             test_data = self.result[i]  # 对列表的每个元素进行拆分，拆分后又是列表格式
             url = test_data[1]
@@ -38,5 +38,5 @@ class Run:
 write=WriteMysql()
 write.write_Mysql(ReadExcel("config.conf").read_excel())
 result=ReadMysql("test_case").read_Mysql()
-run_result=Run(result)
-run_result.run()
+run_result=RunMysql(result)
+run_result.run_mysql()
