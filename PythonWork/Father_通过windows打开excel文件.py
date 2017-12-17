04869d3cc84a8aa5eb7ad4961b2b206abd54a975
zwnpepsi <1076168822@qq.com>
2017-12-17 08:23:43 +0800

    更新作业内容

--- PythonWork/Father_通过windows打开excel文件.py
+++ PythonWork/Father_通过windows打开excel文件.py
@@ -21,9 +21,10 @@ def sortRename():
         number=table.col_values(0) #第1列序号
         name=table.col_values(1) #第2列名称
         os.chdir(directoryname) #指定目录位置
+        name2 = os.listdir()
         for i in range(len(name)):
-            for j in range(len(name)+1):
-                name2=os.listdir()    #定义指定目录下所有文件或文件夹的名字
+            for j in range(len(name2)):
+                   #定义指定目录下所有文件或文件夹的名字
                 if str(name2[j]).encode('utf-8')==str(name[i]).encode('utf-8'):  #判断指定文件夹名称与excel文档中名称相等
                     newname1=int(number[i]),'-',name2[j]    #定义新名字为序号+“-”+原名字，新名字类型为元组
                     newname2=""                             #定义最终新名字为字符串
